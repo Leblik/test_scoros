7f8a88823bde70e764fe0b57a855d9e6e0b4f658
Alexey <leblik87@gmail.com>
2022-05-31 01:28:12 +0400

    Make workable version. Run dif_code in module dif_files

--- dif_files/dif_code.py
+++ dif_files/dif_code.py
@@ -1,9 +1,16 @@
-import sys
-
 def dif_files():
-    print("Start reading files")
+    with open('inFile_1.csv', 'r') as inFile_1:  # Read input files
+        with open('inFile_2.csv', 'r') as inFile_2:
+            file_1, file_2 = set(inFile_1), set(inFile_2)
+            dif_1 = sorted(file_1 - file_2)  # find difference inFile_1 & inFile_2
+            dif_2 = sorted(file_2 - file_1)  # find difference inFile_2 & inFile_1
+
+    with open('outFile_1.csv', 'w') as outFile_1:
+        with open('outFile_2.csv', 'w') as outFile_2:
+            for line_1, line_2 in zip(dif_1, dif_2):
+                outFile_1.write(line_1)
+                outFile_2.write(line_2)
 
 
-dif_files()
-
-print("Close all files")
+if __name__ == '__main__':
+    dif_files()
--- examples.py
+++
@@ -1,3 +0,0 @@
-with open('test.txt', 'wt', encoding='utf-8') as inFile: # Открытие файла
-    words = input() # Получение данных от пользователя
-    inFile.write (words) # Запись данных
